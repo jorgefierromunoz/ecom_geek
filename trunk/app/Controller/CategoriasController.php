793a84e22c2652d8c312a2d32fbfacbcb182b344
jorgefierromunoz@gmail.com <jorgefierromunoz@gmail.com@3d2c82a7-f0fb-bea0-518e-450021b387b4>
2014-07-08 03:54:30 +0000

    -Categorias: vista Editar eliminar agregar y listar listos

--- trunk/app/Controller/CategoriasController.php
+++ trunk/app/Controller/CategoriasController.php
@@ -13,7 +13,52 @@
 class CategoriasController extends AppController{
     //put your code here
     public $name = 'Categorias';
-    public $scaffold;
+        
+    public function index(){
+      
+    }
+    function listacategorias() {
+        $this->set('categorias', $this->Categoria->find('all'));
+        $this->layout = 'ajax';
+    }
+     public function add() {
+         if ($this->request->is('post')) {
+            if (!empty($this->data)) {
+                $this->Categoria->create();
+                if ($this->Categoria->save($this->data)) {
+                   $this->set('categorias','1');
+                }else{
+                     $this->set('categorias','0');
+                }
+            }
+         }
+         $this->layout = 'ajax';
+    }
+      public function view($id = null) {
+        $this->Categoria->id = $id;
+        $this->Categoria->recursive = -1;
+        $this->set('categorias', $this->Categoria->read());
+        $this->layout = 'ajax';
+    }
+    function edit($id = null) {
+        $this->Categoria->id = $id;
+        if ($this->Categoria->save($this->request->data)) {
+            $this->set('categorias', '1');
+        }else{
+            $this->set('categorias', '0');
+        }
+        $this->layout = 'ajax';
+    }
+    
+    function delete($id) {
+        if ($this->Categoria->delete($id)) {
+            $this->set('categorias', '1');
+        }else{
+            $this->set('categorias', '0');
+        }
+            $this->layout = 'ajax';
+        }
+    
 }
 
 ?>
