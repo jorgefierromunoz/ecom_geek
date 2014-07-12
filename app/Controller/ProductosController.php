a1aa107328f42f331a7ebe0f63bd4accfb3d52e0
jorgefierromunoz@gmail.com <jorgefierromunoz@gmail.com@3d2c82a7-f0fb-bea0-518e-450021b387b4>
2014-07-12 07:03:23 +0000

    -fotos img/Fotos/filename

--- app/Controller/ProductosController.php
+++ app/Controller/ProductosController.php
@@ -6,14 +6,64 @@
  */
 
 /**
- * Description of CategoriasController
+ * Description of ProductosController
  *
  * @author MASTER
  */
 class ProductosController extends AppController{
     //put your code here
     public $name = 'Productos';
-    public $scaffold;
+    public function index(){
+      
+    }
+    function listaproductos() {
+        $this->set('productos', $this->Producto->find('all'));
+        $this->layout = 'ajax';
+    }
+     function listaproductosComboBox() {
+        $this->set('productos', $this->Producto->find('all',array('recursive'=>-1)));
+        $this->layout = 'ajax';
+    }
+     public function add() {
+         if ($this->request->is('post')) {
+            if (!empty($this->data)) {
+                $this->Producto->create();
+                if ($this->Producto->save($this->data)) {
+                   $this->set('productos','1');
+                }else{
+                     $this->set('productos','0');
+                }
+            }
+         }
+         $this->layout = 'ajax';
+    }
+      public function view($id = null) {
+        $this->Producto->id = $id;
+        $this->Producto->recursive = -1;
+        $this->set('productos', $this->Producto->read());
+        $this->layout = 'ajax';
+    }
+    function edit($id = null) {
+        $this->Producto->id = $id;
+        if ($this->Producto->save($this->request->data)) {
+            $this->set('productos', '1');
+        }else{
+            $this->set('productos', '0');
+        }
+        $this->layout = 'ajax';
+    }
+    
+    function delete($id) {
+        $cant=$this->Producto->hasSubCat($id);
+        if ($cant==0){
+            $this->Producto->delete($id);
+            $this->set('productos', 't');
+        }else{
+            $this->set('productos', $cant);
+        }
+        $this->layout = 'ajax';
+    }
+    
 }
 
 ?>
